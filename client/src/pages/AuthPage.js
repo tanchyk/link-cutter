d6c741c6e30da04da5d19d1f2c8e9655b1aff66a
tanchyk <tanchyk.bohdan@gmail.com>
2020-08-10 22:57:47 +0300

    Creating an authorization page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const AuthPage = () => {
-    return(
-      <div>
-          <h1>AuthPage</h1>
-      </div>
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const  changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    return (
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Link Cutter</h1>
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div className="input-field">
+                            <input
+                                placeholder="Enter Email"
+                                id="email"
+                                type="text"
+                                className="yellow-input"
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="input-field">
+                            <input
+                                placeholder="Enter Password"
+                                id="password"
+                                type="text"
+                                className="yellow-input"
+                                onChange={changeHandler}
+                            />
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn yellow darken-4" style={{marginRight: 10}}>Log In</button>
+                        <button className="btn grey darken-4">Sign In</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
